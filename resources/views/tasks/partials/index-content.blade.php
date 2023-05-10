d068d8863685155c161d6723f9df798c383de1f1
Luis Leguizamón <lleguizamonl@gmail.com>
2023-05-10 02:21:45 -0400

    Style: Adjust position of buttons in list view

--- resources/views/tasks/partials/index-content.blade.php
+++ resources/views/tasks/partials/index-content.blade.php
@@ -28,15 +28,15 @@
                             <span class="bg-gray-100 text-gray-500 text-xs font-normal mr-2 px-1 py-0.5 rounded">
                                 {{ $task->id }}
                             </span>
-                            <p class="mt-5">
-                                <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">
-                                    <svg class="w-4 h-4 mx-auto hover:text-gray-600" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"></path>
-                                      </svg>
-                                </a>
-                            </p>
-                            <p class="mt-5">
-                                <div class="border border-none rounded-lg p-2">
+                            <div class="mt-5 grid grid-cols-12">
+                                <div class="my-auto">
+                                    <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">
+                                        <svg class="w-4 h-4 mx-auto hover:text-gray-600" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"></path>
+                                          </svg>
+                                    </a>
+                                </div>
+                                <div class="my-auto border border-none rounded-lg p-2">
                                     <form id="delete-form-{{$task->id}}" method="POST"
                                             action="{{ route('tasks.destroy', $task->id) }}">
                                         @csrf
@@ -49,7 +49,7 @@
                                         </button>
                                     </form>
                                 </div>
-                            </p>
+                            </div>
                         </li>
                     @endforeach
                 @else
