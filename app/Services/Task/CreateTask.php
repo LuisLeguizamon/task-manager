f364ed88fe90ef027fc4a18aabc5d1bec2614557
Luis Leguizamón <lleguizamonl@gmail.com>
2023-05-21 14:41:08 -0400

    Refactor: Move sort tasks logic to a helper

--- app/Services/Task/CreateTask.php
+++ app/Services/Task/CreateTask.php
@@ -2,26 +2,17 @@
 
 namespace App\Services\Task;
 
+use App\Helpers\TaskHelper;
 use App\Models\Task;
 
 class CreateTask
 {
     public function execute(array $validated): void
     {
-        $this->orderTasks();
-
+        TaskHelper::sortTasks();
         $task = new Task();
         $task->name = $validated['name'];
         $task->priority = 1;
         $task->save();
-    }
-
-    private function orderTasks()
-    {
-        $tasks = Task::all();
-        foreach ($tasks as $task) {
-            $task->priority = $task->priority + 1;
-            $task->save();
-        }
     }
 }
