f8090ede10f2132c086bf5090636c95b7fa78c12
Luis Leguizamón <lleguizamonl@gmail.com>
2023-05-23 13:34:34 -0400

    Adjust: Remove unused vue file

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,20 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import TaskList from '../components/Tasks/TaskList.vue';
-import TaskForm from '../components/Tasks/TaskForm.vue';
 
 const routes = [
     {
         path: "/tasks",
         component: TaskList
-    },
-    {
-        path: "/tasks/create",
-        component: TaskForm
-    },
-    {
-        path: "/tasks/edit/:id",
-        component: TaskForm
     },
 ];
 
