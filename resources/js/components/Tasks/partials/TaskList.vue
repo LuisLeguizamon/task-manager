cc33a5fbff8280105a9961f771140d2059cb3fc7
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-01 13:54:27 -0400

    Migration: Add completed column to task table

--- resources/js/components/Tasks/partials/TaskList.vue
+++ resources/js/components/Tasks/partials/TaskList.vue
@@ -9,23 +9,31 @@ const emit = defineEmits(['task-deleted', 'task-updated']);
 
 const editInputRefs = ref([]);
 
-function allowEdition(index) {
+// Check task
+function checkTask(taskId) {
+    alert("mark as completed " + taskId)
+}
+
+// Edit task
+function editTask(index) {
     disableEditionOfTasks();
     props.tasks[index].edit_item = true;
     focusItem(index);
 };
 
-async function focusItem(index)
-{
-    await nextTick();//need to use this utility to wait that the element with v-if (the edit input) is "updated" in DOM
-    editInputRefs.value[index].children[1].focus();//the second element (children[1]) is the edit input
+function updateTask(taskId, index) {
+    axios.put('/api/tasks/' + taskId, props.tasks[index])
+        .then(() => {
+            emit('task-updated');
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Error");
+        })
 }
 
-function checkTask(taskId) {
-    alert("mark as completed " + taskId)
-}
-
-function deleteItem(taskId) {
+// Delete task
+function deleteTask(taskId) {
     axios.delete('/api/tasks/' + taskId)
         .then((response) => {
             if (response.data.error) {
@@ -40,22 +48,18 @@ function deleteItem(taskId) {
         })
 };
 
+// Aux functions
+async function focusItem(index)
+{
+    await nextTick();//need to use this utility to wait that the element with v-if (the edit input) is "updated" in DOM
+    editInputRefs.value[index].children[1].focus();//the second element (children[1]) is the edit input
+}
+
 function disableEditionOfTasks() {
     props.tasks.forEach((task) => {
         task.edit_item = false;
     })
 };
-
-function updateTask(taskId, index) {
-    axios.put('/api/tasks/' + taskId, props.tasks[index])
-        .then(() => {
-            emit('task-updated');
-        })
-        .catch((error) => {
-            console.log(error);
-            alert("Error");
-        })
-}
 
 </script>
 <template>
@@ -90,7 +94,7 @@ function updateTask(taskId, index) {
                         Update
                     </button>
                     <button v-else
-                            @click="allowEdition(index)">
+                            @click="editTask(index)">
                         <svg class="w-4 h-4 mx-auto hover:text-gray-600" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"></path>
                         </svg>
@@ -98,7 +102,7 @@ function updateTask(taskId, index) {
                 </div>
                 <!-- Delete task -->
                 <div class="my-auto text-right">
-                    <button @click="deleteItem(task.id)">
+                    <button @click="deleteTask(task.id)">
                         <svg
                             class="w-4 h-4 mx-auto text-red-500 hover:text-red-900" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
