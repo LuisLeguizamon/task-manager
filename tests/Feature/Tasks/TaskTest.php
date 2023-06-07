9146e8fe652f7439efe8e3ebd75cc796ddd63145
Luis Leguizamón <lleguizamonl@gmail.com>
2023-06-07 13:26:21 -0400

    New: Test tasks can be created

--- tests/Feature/Tasks/TaskTest.php
+++ tests/Feature/Tasks/TaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Tasks;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,5 +15,18 @@ class TaskTest extends TestCase
         $response = $this->get('/tasks');
 
         $response->assertStatus(200);
+    }
+
+    public function test_tasks_can_be_created(): void
+    {
+        $form = ['name' => 'Task Test'];
+
+        $response = $this->post(route('tasks.store', $form));
+
+        $response->assertStatus(200);
+
+        $newTask = Task::where('name', $form['name']);
+
+        $this->assertTrue($newTask->exists());
     }
 }
