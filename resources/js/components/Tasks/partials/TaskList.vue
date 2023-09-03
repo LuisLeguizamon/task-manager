a7d6d7317dc3b24fad20e94c3ad7f02873b15f5b
Luis Leguizamón <lleguizamonl@gmail.com>
2023-09-02 20:21:54 -0400

    New: Press the circle to check the task

--- resources/js/components/Tasks/partials/TaskList.vue
+++ resources/js/components/Tasks/partials/TaskList.vue
@@ -5,12 +5,25 @@ const props = defineProps({
     tasks: Array,
 });
 
+const emit = defineEmits(['task-updated']);
+
+// Check task
+function checkTask(taskId) {
+    axios.put('/api/tasks/complete/' + taskId,)
+        .then(() => {
+            emit('task-updated');
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Error");
+        })
+}
 </script>
 <template>
     <div class="bg-white drop-shadow-md rounded mb-5 px-3 py-5" v-for="task in props.tasks">
         <div class="flex items-center">
-            <span>
-                <CircleIcon></CircleIcon>
+            <span class="">
+                <CircleIcon @click="checkTask(task.id)" />
             </span>
             <span class="ml-5">
                 {{ task.name }}
